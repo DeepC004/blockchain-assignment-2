a6d0985e1039fd9450098ac3f3ba50e3246722d3
Deep Chordia <73215996+DeepC004@users.noreply.github.com>
2022-11-15 22:35:46 +0530

    Update Transact.sol

--- Transact.sol
+++ Transact.sol
@@ -2,31 +2,34 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Transact{
-    uint public value;
-    address payable public buyer;
-    address payable public seller;
-    enum State{ Make, Acquire, Release, Inactive}
+    uint public value; //stores the value of the item 
+    address payable public buyer; //stores address of buyer
+    address payable public seller; //stores address of seller
+    enum State{ Make, Acquire, Release, Inactive} //an enumeration of states to capture the state changes at various stages of transaction
     State public state;
 
-    event Aborted(address buyer);
-    event PurchaseConfirmed(address buyer, uint amount);
-    event ItemReceived(address buyer, uint amount);
-    event SellerRefunded(address seller, uint amount);
+    event Aborted(address buyer); //event emitted when a transaction is aborted
+    event PurchaseConfirmed(address buyer, uint amount); //event emitted when purchase of item is confirmed
+    event ItemReceived(address buyer, uint amount); //event emitted when item is received by the buyer
+    event SellerRefunded(address seller, uint amount); //event emitted when seller gets refunded as soon as item receival confirmation takes place
 
     constructor() payable{
         seller = payable(msg.sender);
         value = msg.value / 2;
-        require(msg.value == (2 * value), "The value has to be even.");
+        require(msg.value == (2 * value), "The value has to be even."); //both buyer and seller sending 2x the value of the item as a security deposit
     }
 
-    function about() public {
+    //aborts the transaction if the buyer does not want to purchase the item; only executable by buyer
+    //refunds seller his money and buyer money forfeited 
+    function abort() public {
         require(msg.sender == buyer, "Only the buyer can call this functionality.");
-        require(state == State.Make, "Invalid state.");
+        require(state == State.Make || state == State.Acquire, "Invalid state.");
         emit Aborted(buyer);
         state = State.Inactive;
         seller.transfer(address(this).balance);
     }
 
+    //confirms item purchase; only executable by buyer
     function confirmPurchase() public payable{
         require(msg.value == (2 * value));
         require(state == State.Make, "Invalid state.");
@@ -34,7 +37,9 @@ contract Transact{
         buyer = payable(msg.sender);
         state = State.Acquire;
     }
-
+    
+    //confirms item received; only executable by buyer
+    //seller gets refunded as soon as the item receival is confirmed
     function confirmItemReceived() public{
         require(msg.sender == buyer, "Only the buyer can call this functionality.");
         require(state == State.Acquire, "Invalid state.");
